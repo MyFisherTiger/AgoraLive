e9254d81152b55b7414a3c6e6520f24b889fda14
CavanSu <403029552@qq.com>
2020-07-21 11:58:23 +0800

    update shell

--- AgoraLive-iOS/Build/shell/bugly.sh
+++ AgoraLive-iOS/Build/shell/bugly.sh
@@ -1,10 +1,24 @@
-Product_Path=$1
-App_Version=$2
-App_Name=$3
-BundleId=$4
+Project_Path=$1
+Product_Path=$2
 
-APP_KEY=$5
-APP_ID=$6
+APP_KEY=$3
+APP_ID=$4
+
+Current_Path=`pwd`
+
+cd ${Project_Path}
+
+Project_Name=`find . -name *.xcodeproj | awk -F "[/.]" '{print $(NF-1)}'`
+
+echo "Project_Name" ${Project_Name}
+
+App_Version=`sed -n '/MARKETING_VERSION/{s/MARKETING_VERSION = //;s/;//;s/^[[:space:]]*//;p;q;}' ./${Project_Name}.xcodeproj/project.pbxproj`
+BundleId=`sed -n '/PRODUCT_BUNDLE_IDENTIFIER/{s/PRODUCT_BUNDLE_IDENTIFIER = //;s/;//;s/^[[:space:]]*//;p;q;}' ./${Project_Name}.xcodeproj/project.pbxproj`
+
+echo "App_Version" ${App_Version}
+echo "BundleId" ${BundleId}
+
+cd ${Current_Path}
 
 cd ${Product_Path}
 
@@ -24,10 +38,13 @@ echo `pwd`
 for I in `ls`
 do
     echo "dsym ls" $I
-    rm -f dSYMs.zip
-    zip -q -r dSYMs.zip $I
+    rm -f upload.zip
 
-    curl -k "https://api.bugly.qq.com/openapi/file/upload/symbol?app_key=${APP_KEY}&app_id=${APP_ID}" --form "api_version=1" --form "app_id=${APP_ID}" --form "app_key=${APP_KEY}" --form "symbolType=2"  --form "bundleId=${BundleId}" --form "productVersion=${App_Version}" --form "fileName=dSYMs.zip" --form "file=@dSYMs.zip" --verbose
+    if [[ $I =~ "archive" ]] 
+    then
+    zip -q -r upload.zip $I
+    curl -k "https://api.bugly.qq.com/openapi/file/upload/symbol?app_key=${APP_KEY}&app_id=${APP_ID}" --form "api_version=1" --form "app_id=${APP_ID}" --form "app_key=${APP_KEY}" --form "symbolType=2"  --form "bundleId=${BundleId}" --form "productVersion=${App_Version}" --form "fileName=upload.zip" --form "file=@upload.zip" --verbose
+    fi
 done
 
 
