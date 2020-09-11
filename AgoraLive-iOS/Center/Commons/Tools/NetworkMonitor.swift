34cf07c909e1de8ed510f80352f543fa81643970
CavanSu <403029552@qq.com>
2020-09-11 14:45:14 +0800

    add net connection log

--- AgoraLive-iOS/Center/Commons/Tools/NetworkMonitor.swift
+++ AgoraLive-iOS/Center/Commons/Tools/NetworkMonitor.swift
@@ -13,11 +13,19 @@ import Alamofire
 
 typealias ReachabilityStatus = NetworkReachabilityManager.NetworkReachabilityStatus
 
-class NetworkMonitor: NSObject {
+class NetworkMonitor: NSObject, AGELogBase {
     private lazy var netListener = NetworkReachabilityManager(host: self.host)
     private let host: String
     
     var connect = PublishRelay<ReachabilityStatus>()
+    
+    var logTube: LogTube {
+        get {
+            return ALCenter.shared().centerProvideLogTubeHelper()
+        }
+        set {
+        }
+    }
     
     init(host: String) {
         self.host = host
@@ -48,10 +56,19 @@ extension NetworkMonitor {
         case .on:
             netListener.listener = { [unowned self] status in
                 self.connect.accept(status)
+                self.log(info: "connection status: \(status.description)")
             }
             netListener.startListening()
         case .off:
             netListener.stopListening()
         }
+    }
+}
+
+// MARK: Log
+private extension NetworkMonitor {
+    func log(info: String, extra: String? = nil, funcName: String = #function) {
+        let className = NetworkMonitor.self
+        logOutputInfo(info, extra: extra, className: className, funcName: funcName)
     }
 }
