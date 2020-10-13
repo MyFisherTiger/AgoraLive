68a9f6f416d8797f8b8dbb19c35ef9f8dfb4519e
brianxcli <neteasy1210@gmail.com>
2020-10-13 16:24:50 +0800

    update script for bugly support

--- AgoraLive-Android/build-templates/replace.py
+++ AgoraLive-Android/build-templates/replace.py
@@ -7,6 +7,7 @@ def main():
     keystore = ""
     password = ""
     alias = ""
+    bugly = ""
 
     if "keystore" in os.environ:
         keystore = os.environ["keystore"]
@@ -17,15 +18,27 @@ def main():
     if "alias" in os.environ:
         alias = os.environ["alias"]
 
-    f = open("./app/build.gradle", 'r+')
-    content = f.read()
+    if "bugly" in os.environ:
+        bugly = os.environ["bugly"]
+
+    f1 = open("./app/build.gradle", 'r+')
+    content = f1.read()
     contentNew = re.sub(r'azure_keystore_file', keystore, content)
     contentNew = re.sub(r'azureKeystorePassword', password, contentNew)
     contentNew = re.sub(r'azure_keystore_alias', alias, contentNew)
-    f.seek(0)
-    f.write(contentNew)
-    f.truncate()
+    f1.seek(0)
+    f1.write(contentNew)
+    f1.truncate()
+    f1.close()
 
+    f2 = open("./app/src/main/res/values/strings.xml", 'r+')
+    content = f2.read();
+    replace = '<string name="bugly_app_id">' + bugly + '</string>'
+    contentNew = re.sub(r'<string name="bugly_app_id"></string>', replace, content)
+    f2.seek(0)
+    f2.write(contentNew)
+    f2.truncate()
+    f2.close()
 
 if __name__ == "__main__":
     main()
